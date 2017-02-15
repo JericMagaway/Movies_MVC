52705f346fe7064d8bcb125710eaa4b6feb62e24
cloudfiers <jericmagaway@yahoo.com.ph>
2017-02-15 14:31:19 +0800

    first commit

---
+++ Movies/Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Models
+{
+    public class Movies
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
