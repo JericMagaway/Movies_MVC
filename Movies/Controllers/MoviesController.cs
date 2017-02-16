226d1652a456166d01f076399f03621be5d479f7
cloudfiers <jericmagaway@yahoo.com.ph>
2017-02-16 09:38:51 +0800

    first commit

---
+++ Movies/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //
+        // GET: /Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "WareHouse" };
+            var customers = new List<Customer>
+
+            {
+                new Customer = { Name = "Customer 1"},
+                new Customer = { Name = "Customer 2"}
+
+
+            };
+
+            return View(movie);
+       
+        }
+
+
+
+
+
+
+        /*
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //Movies/Index?pageIndex=2&sortBy=named 
+        public ActionResult Index(int?  pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy ));
+        }*/
+
+
+
+	}
+}
