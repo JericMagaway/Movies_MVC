0558c758780095cd5dab4abfd622db4840204e83
cloudfiers <jericmagaway@yahoo.com.ph>
2017-02-17 11:32:48 +0800

    changes Database

---
+++ Movies/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        
+        //
+        // GET: /Customers/
+        public ActionResult Index()
+        {
+            var customers = _context.Cutomers.ToList();
+            return View(customers);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customer = _context.Cutomers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+       
+
+        
+	}
+}
